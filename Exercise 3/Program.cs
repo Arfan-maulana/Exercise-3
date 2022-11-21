ca9d1d4691c9a795be8e3e39519b710706fa1dd3
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-11-21 15:34:31 +0700

    Add project files.

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    class Node
+    {
+        // create nodes for circular nexted list
+        public int rollNumber;
+        public string name; 
+        pub
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
