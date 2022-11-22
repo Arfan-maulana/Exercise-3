27f2820f06b13d6a6d80bceff5fe0a80a41e8c6e
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-11-22 15:16:13 +0700

    memperbaiki error

--- Exercise 3/Program.cs
+++ Exercise 3/Program.cs
@@ -8,30 +8,126 @@ namespace Exercise_3
 {
     class Node
     {
+
         // create nodes for circular nexted list
         public int rollNumber;
         public string name;
         public Node next;
+        public Node prev;
     }
     class CircularList
     {
         Node LAST;
+        Node START;
         public CircularList()
         {
             LAST = null;
         }
+
+        public void List()
+        {
+            START = null;
+        }
+
+        public void addNote() //add a node in the list 
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.rollNumber = rollNo;
+            newNode.name = nm;
+            // if the node to be interested is the firts node
+            if (LAST == null)
+            {
+                newNode.next = LAST;
+                LAST = newNode;
+                return;
+            }
+            else if (rollNo <= LAST.rollNumber)
+            {
+                if (LAST != null && rollNo == LAST.rollNumber)
+                {
+                    Console.WriteLine("\n Dupliacte student number are not allowed \n");
+                    return;
+                }
+                newNode.next = LAST;
+                LAST = newNode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = LAST; current != null && rollNo >= current.rollNumber; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine("\nDulicate roll numbers not allowed");
+                    return;
+                }
+            }
+            newNode.next = current;
+            newNode.prev = previous;
+
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
+        }
         public bool Search(int rollNo, ref Node previous, ref Node current)
         {
-            for (previous = current = LAST.next; current != LAST; previous=current, current = current.next)
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
             {
-                if(rollNo == current.rollNumber)
-                return (true);
+                if (rollNo == current.rollNumber)
+                    return (true);
             }
             if (rollNo == LAST.rollNumber)
                 return true;
             else
                 return (false);
         }
+        public bool delnode(int rollNo)
+        {
+            Node previous, current;
+            previous = current = null;
+            if (Search(rollNo, ref previous, ref current) == false)
+                return false;
+            if (rollNo == LAST.next.rollNumber)
+            {
+                current = LAST.next;
+                LAST.next = current.next;
+                return true;
+            }
+            if (rollNo <= LAST.rollNumber)
+            {
+                current = LAST.next;
+                previous = LAST.next;
+                while (rollNo > current.rollNumber || previous == LAST)
+                {
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+            }
+            if (rollNo == LAST.rollNumber)
+            {
+                current = LAST;
+                previous = current.next;
+                while (previous.next != LAST)
+                    previous = previous.next;
+                previous.next = LAST.next;
+                LAST = previous;
+                return true;
+            }
+            return true;
+        }
+
+
         public bool listEmty()
         {
             if (LAST == null)
@@ -39,7 +135,7 @@ namespace Exercise_3
             else
                 return false;
         }
-        public bool traverse()
+        public void traverse()
         {
             if (listEmty())
                 Console.WriteLine("\nList is empty");
@@ -53,7 +149,7 @@ namespace Exercise_3
                     Console.Write(curretNode.rollNumber + " " + curretNode.name + "\n");
                     curretNode = curretNode.next;
                 }
-                Console.Write(LAST.rollNumber+ " "+ LAST.name + "\n");
+                Console.Write(LAST.rollNumber + " " + LAST.name + "\n");
             }
         }
         public void firstNode()
@@ -61,74 +157,101 @@ namespace Exercise_3
             if (listEmty())
                 Console.WriteLine("\nlist is empty");
             else
-                Console.WriteLine("\n ]The first record in the list is:\n\n"+ LAST.next.rollNumber+" "+ LAST.next.name);
+                Console.WriteLine("\n ]The first record in the list is:\n\n" + LAST.next.rollNumber + " " + LAST.next.name);
 
         }
-        static void Main(string[] args)
+        class Program
         {
-            CircularList obj = new CircularList();
-            while(true)
+            static void Main(string[] args)
             {
-                try
+                CircularList obj = new CircularList();
+                while (true)
                 {
-                    Console.WriteLine("\nMenu");
-                    Console.WriteLine("1. View all the record in the list");
-                    Console.WriteLine("2. Seacrh for a record in the list ");
-                    Console.WriteLine("3. Dispalay the  record in the list ");
-                    Console.WriteLine("4. Exit");
-                    Console.WriteLine("\nEnter your choice (1-4) ");
-                    char ch = Convert.ToChar(Console.ReadLine());
-                    switch(ch)
+                    try
                     {
-                        case '1':
-                            {
-                                obj.traverse();
-                            }
-                            break;
-                        case '2':
-                            {
-                                if (obj.listEmty()== true)
+                        Console.WriteLine("\nMenu");
+                        Console.WriteLine("1. Add a record to the list");
+                        Console.WriteLine("2. Delete a record from the list");
+                        Console.WriteLine("3. View all records in the list");
+                        Console.WriteLine("4. Search for a record in the list");
+                        Console.WriteLine("5. Display the first record in the list");
+                        Console.WriteLine("6. Exit\n");
+                        Console.WriteLine("Enter your choice (1-6): ");
+                        char ch = Convert.ToChar(Console.ReadLine());
+                        switch (ch)
+                        {
+                            case '1':
                                 {
-                                    Console.WriteLine("\nList is empty");
-                                    break;
+                                    obj.addNote();
                                 }
-                                Node prev, curr;
-                                prev = curr = null;
-                                Console.Write("\nEntrer the roll number of the student to be searched");
-                                int num = Convert.ToInt32(Console.ReadLine());
-                                if (obj.Search(num, ref prev, ref curr) == false)
-                                    Console.WriteLine("\nRecord not found");
-                                else
-                                {
-                                    Console.WriteLine("\nRecord found ");
-                                    Console.WriteLine("\nRoll number: "+ curr.rollNumber);
-                                    Console.WriteLine("\nName: " + curr.name);
-                                }
-                            }
-                            break;
-                        case '3':
-                            {
-                                obj.firstNode();
-                            }
-                            break;
-                        case '4':
-                            return;
-                        default:
-                            {
-                                Console.WriteLine("Invalid option");
                                 break;
-                            }
+                            case '2':
+                                {
+                                    if (obj.listEmty())
+                                    {
+                                        Console.WriteLine("\nList is empty");
+                                        break;
+                                    }
+                                    Console.Write("Enter the roll number of the student" + "whose record is to be deleted: ");
+                                    int StudentNo = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine();
+                                    if (obj.delnode(StudentNo) == false)
+                                        Console.WriteLine("record not found");
+                                    else
+                                        Console.WriteLine("Record with roll number" + StudentNo + "deleted\n");
+                                }
+                                break;
+                            case '3':
+                                {
+                                    obj.traverse();
+                                }
+                                break;
+                            case '4':
+                                {
+                                    if (obj.listEmty() == true)
+                                    {
+                                        Console.WriteLine("\nList is empty");
+                                        break;
+                                    }
+                                    Node prev, curr;
+                                    prev = curr = null;
+                                    Console.Write("\nEnter the roll number of the student whose record you want to search: ");
+                                    int num = Convert.ToInt32(Console.ReadLine());
+                                    if (obj.Search(num, ref prev, ref curr) == false)
+                                        Console.WriteLine("\nRecord not found");
+                                    else
+                                    {
+                                        Console.WriteLine("\nRecord found");
+                                        Console.WriteLine("\nRoll number: " + curr.rollNumber);
+                                        Console.WriteLine("\nName: " + curr.name);
+                                    }
+                                }
+                                break;
+                            case '5':
+                                {
+                                    obj.firstNode();
+                                }
+                                break;
+                            case '6':
+                                {
+                                    return;
+                                }
+                                break;
+                            default:
+                                {
+                                    Console.WriteLine("\nInvalid option");
+                                }
+                                break;
+                        }
                     }
-
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Check for the values entered.");
+                    }
                 }
-                catch(Exception e)
             }
         }
-    }
+    } }
 
-    
-    
-    
-       
-    
+
 
